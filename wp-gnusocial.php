49897fa88c97889a8fcf0b235a2f19ee6fc8929e
Manuel Ortega <manuel@enkidu.coop>
2015-03-31 10:25:15 +0200

    Remeto de la repondo_url

--- wp-gnusocial.php
+++ wp-gnusocial.php
@@ -30,8 +30,8 @@ if ( ! defined( 'WPINC' ) ) {
  */
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
 
 /**
