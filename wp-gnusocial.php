54422794a64f666c910b5377120770fe7118b4fc
Manuel Ortega <manuel@enkidu.coop>
2015-12-12 14:13:56 +0100

    preparoj por versio 0.2.7

--- wp-gnusocial.php
+++ wp-gnusocial.php
@@ -14,7 +14,7 @@
  * Description: GNU social based comment system for WordPress.
  * Author:      Las Indias
  * Author URI:  http://lasindias.com
- * Version:     0.2.6
+ * Version:     0.2.7
  * Text Domain: wp_gnusocial
  * Domain Path: /languages/
  */
