4d55614d505a1dd8a44b04b2b0edd4690234daf3
Manuel Ortega <manuel@enkidu.coop>
2016-02-26 02:04:42 +0100

    Adapto al wordpress-postuloj

--- includes/wp-gnusocial-klaso.php
+++ includes/wp-gnusocial-klaso.php
@@ -367,7 +367,7 @@ final class Wp_Gnusocial {
       if( !(get_post_meta( $post->ID, 'wpgs_conversation_id', true ) == '') ) {
 
             $konversacio_id = get_post_meta( $post->ID, 'wpgs_conversation_id', true );
-            $pepo_id = get_post_meta( $post->ID, 'wpgs_notice_id', true ));
+            $pepo_id = get_post_meta( $post->ID, 'wpgs_notice_id', true );
 
             if(empty($pepo_id){
                 $pepo_id = $konversacio_id;
